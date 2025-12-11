71ebb67a15e2b1e4bafe36fae7b6cb436f7d1581
Sanjaykumarrangu <sanjayrangu97@gmail.com>
2025-12-11 17:31:12 +0530

    photos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,6 +33,7 @@ export default function Navbar() {
     { label: 'Services', href: '/services' },
     { label: 'Contact', href: '/contact' },
     { label: 'Our Prices', href: '/solarposter' },
+    { label: 'Our Projects', href: '/projects' },
 
     { label: 'Why Choose Us', href: '/why-choose-vortex' },
         { label: 'Solar FAQs', href: '/faq' },
